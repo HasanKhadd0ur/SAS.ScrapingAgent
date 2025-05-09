c209ba525006332ec9ca47f59f71f27ce461dc03
Hasan Khaddour <hasan.b.khaddour@gmail.com>
2025-05-09 10:21:56 +0300

    Add Dummy File Scraper for testing

---
+++ src/app/scrapers/dummy/dummy_file_scrarper.py
@@ -0,0 +1,42 @@
+from app.scrapers.base import BaseScraper
+from app.core.models.scraper_task import ScraperTask
+from app.core.models.message import Message
+from typing import AsyncGenerator, List
+import json
+import asyncio
+import os
+
+class DummyFileScraper(BaseScraper):
+    def __init__(self, config: dict):
+        self.config = config
+        self.file_path = config.get("file_path", "../assets/sample_tweets.jsonl")
+        self.delay = config.get("delay", 0.1)
+        self.batch_size = config.get("batch_size", 1)
+
+    async def run_task(self, task: ScraperTask) -> AsyncGenerator[List[Message], None]:
+        batch = []
+
+        try:
+            with open(self.file_path, "r", encoding="utf-8") as f:
+                for line in f:
+                    await asyncio.sleep(self.delay)  # simulate delay
+                    data = json.loads(line)
+
+                    msg = Message(
+                        source="dummy_file",
+                        domain=task.domain,
+                        channel="test",
+                        base_content=data["text"],
+                        content=data["text"]
+                    )
+                    batch.append(msg)
+
+                    if len(batch) >= self.batch_size:
+                        yield batch
+                        batch = []
+
+                if batch:
+                    yield batch
+
+        except Exception as e:
+            print(f"Error reading dummy file: {e}")
