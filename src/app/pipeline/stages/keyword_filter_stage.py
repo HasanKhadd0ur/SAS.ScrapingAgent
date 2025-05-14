9f0cfc9fe968dd98eea7dfc34a39ec741aa41ed9
Hasan Khaddour <hasan.b.khaddour@gmail.com>
2025-05-14 15:48:55 +0300

    Fix Pipeline , make it async

--- src/app/pipeline/stages/keyword_filter_stage.py
+++ src/app/pipeline/stages/keyword_filter_stage.py
@@ -12,18 +12,15 @@ class KeywordFilterStage(FilterStage):
         super().__init__()
         self.keywords = keywords
 
-    def process(self, messages: List[Message], nextStep: Optional[FilterStage] = None) -> List[Message]:
+    async def process(self, messages: List[Message], nextStep: Optional[FilterStage] = None) -> List[Message]:
         filtered_messages = []
         for message in messages:
             keywords = self.keywords.get(message.domain, [])
             content = message.content.lower()
 
             if any(k in content for k in keywords):
-                if nextStep:
-                    next_result = nextStep.process([message])
-                    if next_result:
-                        filtered_messages.extend(next_result)
-                else:
-                    filtered_messages.append(message)
-
+                
+                filtered_messages.append(message)
+        if nextStep: 
+            return await nextStep.process(filtered_messages)
         return filtered_messages
--- src/app/pipeline/stages/messages_publishing_stage.py
+++ src/app/pipeline/stages/messages_publishing_stage.py
@@ -1,8 +1,10 @@
+import asyncio
 from typing import List, Optional
 from app.core.models.message import Message
 from app.core.services.logging_service import LoggingService
 from app.pipeline.base import FilterStage
 from app.core.services.messages_service import MessagesService
+from app.kafka.kafka_producer import KafkaProducer
 
 logger = LoggingService("MessagesPublishingStage").get_logger()
 
@@ -10,13 +12,13 @@ class MessagesPublishingStage(FilterStage):
     def __init__(self):
         super().__init__()
         self.messagesService = MessagesService()
+        self.messagesService.set_kafka_producer(KafkaProducer())
 
-    def process(self, messages: List[Message], nextStep: Optional[FilterStage] = None) -> List[Message]:
-        for message in messages:
-            try:
-                self.messagesService.publish(message.domain, [message])  # Convert to dict if needed
-            except Exception as e:
-                logger.exception(f"Failed to publish message ID")
+    async def process(self, messages: List[Message], nextStep: Optional[FilterStage] = None) -> List[Message]:
+        try:
+            await self.messagesService.publish(messages[0].domain, messages)
+        except Exception as e:
+            logger.exception(f"Failed to publish messages ")
         if nextStep:
-            return nextStep.process(messages)
+            return await nextStep.process(messages)
         return messages
